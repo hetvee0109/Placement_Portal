4574f1f74e182cb8ed1aa5dccf1ed0bd1ade87c2
hetvee0109 <rabarahetvee@gmail.com>
2026-01-07 00:35:49 +0530

    Initial commit: add StudentProfile backend files

---
+++ backend/src/main/java/com/placement/management/entity/CareerPreference.java
@@ -0,0 +1,6 @@
+package com.placement.management.entity;
+
+public enum CareerPreference {
+    HIGHER_STUDIES,
+    PLACEMENT
+}
---
+++ backend/src/main/java/com/placement/management/repository/StudentProfileRepository.java
@@ -0,0 +1,14 @@
+package com.placement.management.repository;
+
+import com.placement.management.entity.StudentProfile;
+import com.placement.management.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface StudentProfileRepository extends JpaRepository<StudentProfile, Long> {
+
+    Optional<StudentProfile> findByUser(User user);
+
+    boolean existsByUser(User user);
+}
